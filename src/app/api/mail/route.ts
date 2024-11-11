938e1ec7ac1feff1eb299c74c52c608a84651e35
Dev-Odior <godspoweraino@gmail.com>
2024-11-11 23:44:26 +0100

    debug: checking error

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -2,43 +2,43 @@ import { NextRequest, NextResponse } from "next/server";
 import mailService from "./mail.service";
 
 export async function POST(req: NextRequest) {
-    try {
-        // Parse the request body to get the email details
-        const { to, subject, body, templateName, replacements, attachments } =
-            await req.json();
+  try {
+    // Parse the request body to get the email details
+    const { to, subject, body, templateName, replacements, attachments } =
+      await req.json();
 
-        // Set up the mail options
-        const mailOptions = {
-            to,
-            from: "godspoweraino@gmail.com", // Update with your 'from' email address
-            subject,
-            body,
-            templateName,
-            replacements,
-            attachments, // Optional: Add attachments
-        };
+    // Set up the mail options
+    const mailOptions = {
+      to,
+      from: "godspoweraino@gmail.com", // Update with your 'from' email address
+      subject,
+      body,
+      templateName,
+      replacements,
+      attachments, // Optional: Add attachments
+    };
 
-        // Send the email
-        await mailService.sendMail(mailOptions);
+    // Send the email
+    await mailService.sendMail(mailOptions);
 
-        // Respond with a success message
-        return NextResponse.json(
-            { message: "Email sent successfully" },
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error("Error sending email:", error);
-        return NextResponse.json(
-            { error: "Failed to send email" },
-            { status: 500 }
-        );
-    }
+    // Respond with a success message
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return NextResponse.json(
+      { error: `Failed to send email ${error}` },
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET(req: NextRequest) {
-    console.log(req);
-    return NextResponse.json(
-        { message: "Email Template route" },
-        { status: 200 }
-    );
+  console.log(req);
+  return NextResponse.json(
+    { message: "Email Template route" },
+    { status: 200 }
+  );
 }
